987799bf96047d1e83f87430e796d35fb098e722
jerome-918 <565632099@qq.com>
2016-07-05 10:57:00 +0800

    finish the validation of User，Cart，Favorite.

---
+++ src/test/java/action/AddressActionTest.java
@@ -0,0 +1,48 @@
+package action;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zhang on 2016/7/4.
+ */
+public class AddressActionTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void viewAddress() throws Exception {
+
+    }
+
+    @Test
+    public void addAddress() throws Exception {
+
+    }
+
+    @Test
+    public void deleteAddress() throws Exception {
+
+    }
+
+    @Test
+    public void updateAddress() throws Exception {
+
+    }
+
+    @Test
+    public void setDefaultAddress() throws Exception {
+
+    }
+
+}
